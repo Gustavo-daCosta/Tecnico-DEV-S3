4b8ac7da31464508e11f16d2196136d6afed818b
Gustavo-daCosta <gustavo.costa.senaidev@gmail.com>
2024-01-26 16:17:22 -0300

    React Native: Suporte a fontes externas adicionado ao helloworld

--- React Native/helloworld/App.js
+++ React Native/helloworld/App.js
@@ -1,26 +1,56 @@
 import { StyleSheet, Text, TextInput, View, Image, Button, TouchableOpacity } from 'react-native';
 
+import {
+  useFonts,
+  MontserratAlternates_100Thin,
+  MontserratAlternates_200ExtraLight,
+  MontserratAlternates_300Light,
+  MontserratAlternates_400Regular,
+  MontserratAlternates_500Medium,
+  MontserratAlternates_600SemiBold,
+  MontserratAlternates_700Bold,
+  MontserratAlternates_800ExtraBold,
+  MontserratAlternates_900Black,
+} from '@expo-google-fonts/montserrat-alternates';
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Image
-        source={require('./src/assets/VitalHub_Logo.png')}
-      />
+  let [fontsLoaded] = useFonts({
+    MontserratAlternates_100Thin,
+    MontserratAlternates_200ExtraLight,
+    MontserratAlternates_300Light,
+    MontserratAlternates_400Regular,
+    MontserratAlternates_500Medium,
+    MontserratAlternates_600SemiBold,
+    MontserratAlternates_700Bold,
+    MontserratAlternates_800ExtraBold,
+    MontserratAlternates_900Black,
+  });
 
-      <View>
-        <Text style={[styles.heading]}>Entrar ou criar conta</Text>
+  if (!fontsLoaded) {
+    <AppLoading />
+  } else {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={require('./src/assets/VitalHub_Logo.png')}
+        />
+  
+        <View>
+          <Text style={[styles.heading]}>Hello World</Text>
+        </View>
+        <Text>Welcome to the mobile development world</Text>
+  
+        <TextInput
+          style={styles.input}
+          defaultValue='Exemplo de input'
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text>Botão</Text>
+        </TouchableOpacity>
       </View>
-      <Text>Welcome to the mobile development world</Text>
-
-      <TextInput
-        style={styles.input}
-        defaultValue='Exemplo de input'
-      />
-      <TouchableOpacity style={styles.button}>
-        <Text>Botão</Text>
-      </TouchableOpacity>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -36,6 +66,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
     marginBottom: 13,
+    fontFamily: 'MontserratAlternates_500Medium'
   },
   input: {
     width: '90%',
@@ -45,7 +76,10 @@ const styles = StyleSheet.create({
   },
   button: {
     width: '90%',
+    height: 30,
     backgroundColor: 'lightblue',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
   }
 });
